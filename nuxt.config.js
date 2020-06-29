19a164e54382f890da13d955941dd65a935ab17e
soulsam480 <soulsam480@hotmail.com>
2020-06-29 18:21:32 +0530

    commit

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,107 +1,106 @@
-import blogs from './content/blogs.json'
+import blogs from "./content/blogs.json"
 
 export default {
-  mode: 'universal',
+  mode: "universal",
 
   /*
    ** Headers of the page
    */
   head: {
-    title: 'Sambit Sahoo',
+    title: "Sambit Sahoo",
     link: [
       {
-        rel: 'dns-prefetch preconnect',
-        href: 'cdn.jsdeliver.net'
-
+        rel: "dns-prefetch preconnect",
+        href: "cdn.jsdeliver.net"
       }
     ],
     meta: [
       {
-        charset: 'utf-8'
+        charset: "utf-8"
       },
       {
-        name: 'viewport',
-        content: 'width=device-width, initial-scale=1'
+        name: "viewport",
+        content: "width=device-width, initial-scale=1"
       },
       {
-        hid: 'og:url',
-        property: 'og:url',
+        hid: "og:url",
+        property: "og:url",
         content: `https://sambitsahoo2.cf`
       },
       {
-        hid: 'og:title',
-        property: 'og:title',
-        content: 'Sambit Sahoo'
+        hid: "og:title",
+        property: "og:title",
+        content: "Sambit Sahoo"
       },
       {
-        hid: 'description',
-        name: 'description',
-        content: 'Sambit Sahoo/ Developer, Dreamer, Enthusiast'
+        hid: "description",
+        name: "description",
+        content: "Sambit Sahoo/ Developer, Dreamer, Enthusiast"
       },
       {
-        hid: 'og:description',
-        property: 'og:description',
-        content: 'Sambit Sahoo/ Developer, Dreamer, Enthusiast'
+        hid: "og:description",
+        property: "og:description",
+        content: "Sambit Sahoo/ Developer, Dreamer, Enthusiast"
       },
       {
-        hid: 'og:image',
-        property: 'og:image',
-        content: 'https://sambitsahoo2.cf/_nuxt/icons/icon_150.ae673c.png'
+        hid: "og:image",
+        property: "og:image",
+        content: "https://sambitsahoo2.cf/_nuxt/icons/icon_150.ae673c.png"
       },
       {
-        hid: 'og:article:author',
-        property: 'og:article:author',
-        content: 'https://github.com/soulsam480'
+        hid: "og:article:author",
+        property: "og:article:author",
+        content: "https://github.com/soulsam480"
       },
       {
-        hid: 'og:site_name',
-        property: 'og:site_name',
-        content: 'Sambit Sahoo'
+        hid: "og:site_name",
+        property: "og:site_name",
+        content: "Sambit Sahoo"
       },
       {
-        hid: 'og:type',
-        property: 'og:type',
-        content: 'website'
+        hid: "og:type",
+        property: "og:type",
+        content: "website"
       },
       {
-        hid: 'twitter:card',
-        name: 'twitter:card',
-        content: 'summary_large_image'
+        hid: "twitter:card",
+        name: "twitter:card",
+        content: "summary_large_image"
       },
       {
-        hid: 'twitter:title',
-        name: 'twitter:title',
-        content: 'Sambit Sahoo'
+        hid: "twitter:title",
+        name: "twitter:title",
+        content: "Sambit Sahoo"
       },
       {
-        hid: 'twitter:description',
-        name: 'twitter:description',
-        content: 'Sambit Sahoo/ Developer, Dreamer,'
+        hid: "twitter:description",
+        name: "twitter:description",
+        content: "Sambit Sahoo/ Developer, Dreamer,"
       }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
   },
 
   /*
    ** Customize the progress-bar color
    */
-  loading: { color: '#fff' },
+  loading: { color: "#fff" },
 
   /**
    * Plugins
    */
-  plugins: ['~/plugins/lazyload'],
+  plugins: ["~/plugins/lazyload"],
 
   /*
    ** Nuxt.js modules
    */
   modules: [
-    ['@nuxtjs/google-analytics', { id: 'UA-161449705-1' }],
-    '@nuxtjs/pwa',
-    '@nuxtjs/sitemap',
-    '@nuxtjs/robots',
-    'nuxt-fontawesome',
-    'nuxt-webfontloader'
+    ["@nuxtjs/google-analytics", { id: "UA-161449705-1" }],
+    "@nuxtjs/pwa",
+    "@nuxtjs/sitemap",
+    "@nuxtjs/robots",
+    "nuxt-fontawesome",
+    "nuxt-webfontloader"
   ],
 
   /**
@@ -109,7 +108,7 @@ export default {
    */
   webfontloader: {
     google: {
-      families: ['Rubik:400,700', 'Slabo+27px:400'] // Loads Lato font with weights 400 and 700
+      families: ["Rubik:400,700", "Slabo+27px:400"] // Loads Lato font with weights 400 and 700
     }
   },
 
@@ -119,21 +118,21 @@ export default {
   fontawesome: {
     imports: [
       {
-        set: '@fortawesome/free-brands-svg-icons',
+        set: "@fortawesome/free-brands-svg-icons",
         icons: [
-          'faTwitter',
-          'faInstagram',
-          'faGithub',
-          'faVuejs',
-          'faReact',
-          'faJs',
-          'faDocker',
-          'faWordpress',
-          'faNodeJs',
-          'faYarn',
-          'faFacebook',
-          'faPython',
-          'faEnvira'
+          "faTwitter",
+          "faInstagram",
+          "faGithub",
+          "faVuejs",
+          "faReact",
+          "faJs",
+          "faDocker",
+          "faWordpress",
+          "faNodeJs",
+          "faYarn",
+          "faFacebook",
+          "faPython",
+          "faEnvira"
         ]
       }
     ]
@@ -143,51 +142,49 @@ export default {
    * Manifest
    */
   manifest: {
-    name: 'Sambit Blog',
-    short_name: 'Sambit Blog',
-    description: 'Sambit Sahoo portfolio PWA',
-    version: '1.0',
-    start_url: '/',
-    scope: '.',
-    background_color: '#4fd6b7',
-    theme_color: '#03fcc2',
-    display: 'standalone',
-    orientation: 'portrait-primary',
-    dir: 'ltr',
-    lang: 'en-US',
+    name: "Sambit Blog",
+    short_name: "Sambit Blog",
+    description: "Sambit Sahoo portfolio PWA",
+    version: "1.0",
+    start_url: "/",
+    scope: ".",
+    background_color: "#4fd6b7",
+    theme_color: "#03fcc2",
+    display: "standalone",
+    orientation: "portrait-primary",
+    dir: "ltr",
+    lang: "en-US",
     related_applications: [
       {
-        platform: 'play',
-        url: 'https://play.google.com/.../app_name',
-        id: 'com.example.app'
+        platform: "play",
+        url: "https://play.google.com/.../app_name",
+        id: "com.example.app"
       }
-     ]
+    ]
   },
-  pwa:{
-      icon:{
-
-          sizes: [ 150, 192, 512]
-
-      }
+  pwa: {
+    icon: {
+      sizes: [150, 192, 512]
+    }
   },
   workbox: {
     runtimeCaching: [
       {
-        urlPattern: 'https://fonts.googleapis.com/.*',
-        handler: 'cacheFirst',
-        method: 'GET',
+        urlPattern: "https://fonts.googleapis.com/.*",
+        handler: "cacheFirst",
+        method: "GET",
         strategyOptions: { cacheableResponse: { statuses: [0, 200] } }
       },
       {
-        urlPattern: 'https://fonts.gstatic.com/.*',
-        handler: 'cacheFirst',
-        method: 'GET',
+        urlPattern: "https://fonts.gstatic.com/.*",
+        handler: "cacheFirst",
+        method: "GET",
         strategyOptions: { cacheableResponse: { statuses: [0, 200] } }
       },
       {
-        urlPattern: 'https://cdn.jsdelivr.net/npm/workbox-cdn@4.3.1/workbox',
-        handler: 'cacheFirst',
-        method: 'GET',
+        urlPattern: "https://cdn.jsdelivr.net/npm/workbox-cdn@4.3.1/workbox",
+        handler: "cacheFirst",
+        method: "GET",
         strategyOptions: { cacheableResponse: { statuses: [0, 200] } }
       }
     ]
@@ -197,17 +194,17 @@ export default {
    * sitemap
    */
   sitemap: {
-    hostname: 'https://example.com',
+    hostname: "https://example.com",
     gzip: true,
-    exclude: ['/admin/']
+    exclude: ["/admin/"]
   },
 
   /**
    * Robots
    */
   robots: {
-    UserAgent: '*',
-    Disallow: '/admin'
+    UserAgent: "*",
+    Disallow: "/admin"
   },
 
   /**
@@ -221,8 +218,8 @@ export default {
    * Transition
    */
   transition: {
-    name: 'fade',
-    mode: 'out-in'
+    name: "fade",
+    mode: "out-in"
   },
 
   /*
@@ -236,16 +233,16 @@ export default {
       // Run ESLint on save
       if (ctx.isDev && ctx.isClient) {
         config.module.rules.push({
-          enforce: 'pre',
+          enforce: "pre",
           test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
+          loader: "eslint-loader",
           exclude: /(node_modules)/
         })
       }
 
       config.module.rules.push({
         test: /\.md$/,
-        loader: 'frontmatter-markdown-loader',
+        loader: "frontmatter-markdown-loader",
         options: {
           vue: true
         }
